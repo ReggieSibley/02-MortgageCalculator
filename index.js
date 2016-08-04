bbcb3c66e06e7c2b256362c66ce0a3ca8b6e78ed
Reggie Sibley <Tre@Reggies-MacBook-Pro.local>
2016-08-03 23:51:24 -0700

    Css styled mortgage calculator

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-/*
-1.Write the following JavaScript in your index.js file
 
-2.Create a function that will be called when the user 
-clicks on the button element you added to your HTML.
-
-3.This function should grab the values entered by the user
- from the input elements and the select element.
-
-4.The function should then calculate the monthly payment as 
-follows (we can break this formula into 4 'buckets' for 
-readability):
-*/
 
 $(document).ready(function() {
 
@@ -29,8 +17,13 @@ $("#main_button").click(function calculateMortgage() {
 	var interestQuotient  = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
 	var monthlyPayment = Math.round((balance * interestQuotient) * 100) / 100;
 
-	$("#mortgageamount").html("Your Monthly Mortgage Rate Will Be " + "$" + monthlyPayment + " dollars");
+
+
+	$("#mortgageamount").html("Your Mortgage Rate Will Be " + "$" + monthlyPayment + " Dollars/Month");
+
+
 
 	});
+	
 
 });
